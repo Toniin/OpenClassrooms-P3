7f85a7ab042dabbf4599cab165098233b039fb0d
Toniin <antonin.chaudiere@hotmail.fr>
2023-02-26 22:18:13 +0100

    Add background shade and modal

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -1,5 +1,6 @@
 import { createWork } from "./works.js";
 import { createFilter } from "./filters.js";
+// import { editorModeIcon } from "../assets/cssAdded/editiorIcon.svg";
 
 // Importation des projets
 fetch("http://localhost:5678/api/works")
@@ -39,6 +40,44 @@ if (tokenSession) {
   // Afficher la barre d'édition avant le header
   const editorBar = document.createElement("div");
   editorBar.classList.add("editorBar");
+
   const header = document.querySelector("header");
   document.querySelector("body").insertBefore(editorBar, header);
+
+  // Afficher le mode édition et le bouton de publication
+  const infoEditorMode = document.createElement("div");
+  const infoEditorModeIcon = document.createElement("p");
+  const infoEditorModeText = document.createElement("p");
+  const btnEditorPublish = document.createElement("button");
+
+  infoEditorMode.classList.add("editorBar-info");
+  btnEditorPublish.classList.add("editorBar-button");
+  infoEditorModeIcon.textContent = "ICON";
+  infoEditorModeText.textContent = "Mode édition";
+  btnEditorPublish.textContent = "publier les changements";
+
+  infoEditorMode.appendChild(infoEditorModeIcon);
+  infoEditorMode.appendChild(infoEditorModeText);
+  editorBar.appendChild(infoEditorMode);
+  editorBar.appendChild(btnEditorPublish);
+
+  // Affichage d'une modale pour éditer les projets
+  btnEditorPublish.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    // Vérifier si l'arrière plan n'existe pas, on le créé
+    // Sinon on le supprime
+    if (document.getElementsByClassName("backgroundModal").length === 0) {
+      const backgroundModal = document.createElement("div");
+      backgroundModal.classList.add("backgroundModal");
+      document.querySelector("body").appendChild(backgroundModal);
+
+      // Création de la modale
+      const modalEditor = document.createElement("div");
+      modalEditor.classList.add("modalEditor");
+      backgroundModal.appendChild(modalEditor);
+    } else {
+      document.getElementsByClassName("backgroundModal")[0].remove();
+    }
+  });
 }
